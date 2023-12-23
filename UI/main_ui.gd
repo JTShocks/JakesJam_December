1b9037413792b9d514f7d87b6c78012f171628dc
Paul-Griffin-1517-1 <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2023-12-23 02:19:12 -0500

    Timer displays properly

--- UI/main_ui.gd
+++ UI/main_ui.gd
@@ -5,7 +5,9 @@ var player_stats := preload("res://Scripts/WaveSystem/PlayerStats.tres")
 @onready var wave_number := $"PanelContainer/VBoxContainer/Wave Number"
 @onready var wave_interval_timer := $"PanelContainer/VBoxContainer/Wave Interval Timer"
 @onready var cash := $PanelContainer/VBoxContainer/Cash
-
+@export var game_controller : Node
+@onready var wave_system  = game_controller.get_node("Wave System")
+var wave_timer : Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +16,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	wave_timer = wave_system.waveIntervalTimer
+	
 	health.text = "Player Health: " + str(player_stats.MaxHealth)
 	wave_number.text = "Wave: " #+ str(insert_wave_number_here)
-	wave_interval_timer.text = "Time Until Next Wave: " #+ str(time)
+	wave_interval_timer.text = "Time Until Next Wave: " + str(snappedf(wave_timer.time_left, 1))
 	cash.text = "Total Cash:" #+ str(cash)
